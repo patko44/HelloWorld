89a93b05ea7e9377371d73ccf9db70d4e40ad4cb
patrik <vojtechpat@gmail.com>
2019-11-23 16:00:09 +0100

    Master commit

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,6 +4,6 @@ public class Main {
 
     public static void main(String[] args) {
 	    System.out.println("Hello World!");
-        System.out.println("New Hello Worldsssss!");
+        System.out.println("New Hello Worldsssssss!");
     }
 }
