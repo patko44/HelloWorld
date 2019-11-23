7b5f0bf768d159b9571f020b178322220ecb322d
patrik <vojtechpat@gmail.com>
2019-11-23 15:56:17 +0100

    Feature second commit

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -6,5 +6,6 @@ public class Main {
 	    System.out.println("Hello World!");
         System.out.println("New Hello World!");
         System.out.println("New Hello World!");
+        System.out.println("New Hello World!");
     }
 }
